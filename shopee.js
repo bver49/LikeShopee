2b0e475e1cfb614ae4e06b96018bc4f1915e5819
Derek <derek@playsport.cc>
2018-07-05 17:36:02 +0800

    auto fill userid and pw

--- shopee.js
+++ shopee.js
@@ -2,10 +2,18 @@ const Nightmare = require('nightmare')
 const nightmare = Nightmare({
     show: true
 })
+var userid = '';
+var pw = '';
+
 var mall = "https://shopee.tw/shopee24h";
 nightmare.goto(mall)
     .wait('.shopee-svg-icon.icon-like-2')
     .click('.shopee-svg-icon.icon-like-2')
+    .wait(2000)
+    .type('.shopee-authen .input-with-status__input[type=text]', userid)
+    .type('.shopee-authen .input-with-status__input[type=password]', pw)
+    .wait(2000)
+    .click('.shopee-authen .shopee-button-solid.shopee-button-solid--primary')
     .wait(25000)
     .evaluate(function () {
         var items = document.querySelectorAll('.shopee-svg-icon.icon-like-2');
